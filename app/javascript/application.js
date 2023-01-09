dd578a9d7a9de762d33a369e05e4a82ce3805b2d
jlmansilla <juanmancillaster@gmail.com>
2023-01-09 20:06:30 -0300

    commit inicial

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+ //= require rails-ujs
+ 
+ 
+ //= require_tree .
+
+//= require bootstrap
+
+import "@hotwired/turbo-rails"
+import "controllers"
+import * as bootstrap from "bootstrap"
